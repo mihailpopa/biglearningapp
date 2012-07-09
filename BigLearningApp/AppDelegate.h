55963f09bd0700cf5829aae9809dab1a806f326c
Mihail Popa <mihail.popa@xoomworks.com>
2012-07-09 15:32:30 +0300

    Initial Commit

---
+++ BigLearningApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BigLearningApp
+//
+//  Created by Mihai Popa on 7/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
